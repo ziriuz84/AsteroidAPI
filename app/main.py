a4217db1efa0d581447320ff8d1ba20e460bfaf3
Sirio Negri <ziriuz84@gmail.com>
2023-05-21 19:18:08 +0200

    Added examples of get routes and models/weather

--- app/main.py
+++ app/main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI, HTTPException
+from app.lib.scheduling import weather
+import models
 
 app = FastAPI(
     title="AsteroidAPI",
     version="0.0.1",
     description="Simple api to request data for asteroids in better format",
 )
+
+
+@app.get("/")
+def root() -> str:
+    return "home"
+
+
+@app.get("/api/v1/scheduling")
+def scheduling():
+    return {"section": "scheduling", "test": "test"}
+
